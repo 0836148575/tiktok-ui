30ffab7117b678a8cddd7dd2b3bd262c6b4c2ac8
ducanh <tranducanh2422000@gmail.com>
2022-07-08 15:13:43 +0700

    Giải quyết vấn đề nhiều file index.js

--- src/components/Popper/Menu/index.js
+++ src/components/Popper/Menu/index.js
@@ -17,7 +17,7 @@ function Menu({ children, items = [], hideOnClick = false, onChange = defaultFn 
     const renderItems = () => {
         return current.data.map((item, index) => {
             const isParent = !!item.children;
-            console.log(item.children);
+
             return (
                 <MenuItem
                     key={index}
